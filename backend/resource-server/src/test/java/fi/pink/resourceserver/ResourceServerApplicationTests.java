c799ec93dec5c77e899f8fe3bc718f969b4e7582
pinkkila <petteri.pinkkila@outlook.com>
2024-12-18 13:26:08 +0200

    Initial commit and initial backend

---
+++ backend/resource-server/src/test/java/fi/pink/resourceserver/ResourceServerApplicationTests.java
@@ -0,0 +1,13 @@
+package fi.pink.resourceserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResourceServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
